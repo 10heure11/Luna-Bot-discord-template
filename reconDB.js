dc3054b4a225e3e551bffe4faf60997d9468137e
10heure11 <82675790+10heure11@users.noreply.github.com>
2021-05-29 16:02:17 -0600

    Update reconDB.js

--- reconDB.js
+++ reconDB.js
@@ -2,7 +2,7 @@ const client = require("./index")
 const { reconDB } = require("reconlx")
 const db = new reconDB(client, {
   uri:
-    "mongodb+srv://mydb:LoLo97234@cluster0.n19fg.mongodb.net/LunaBot",
+    "srgpkhjm",
 })
 
 module.exports = db;
